d37b69df22c5b2d3e39b13e6dd03d2987b93eb74
Robert Pratt <rpratt@atlassian.com>
2020-12-05 21:08:16 -0500

    init client wrapper (incomplete)

---
+++ client/client.go
@@ -0,0 +1,26 @@
+/*
+	@author: Robert Pratt <robert.pratt[at]homelab.farm>
+	@package: client
+	@description: HTTP client wrapper for MassLotto API
+	@created: 2020-12-05
+*/
+package client
+
+import (
+	"net/http"
+	"time"
+)
+
+func CreateLottoClient() (client *http.Client) {
+	tr := &http.Transport{
+		IdleConnTimeout: 30 * time.Second,
+	}
+	client = &http.Client{
+		Transport: tr,
+	}
+	return
+}
+
+func CreateDrawingRequest(game string, date string) {
+
+}
